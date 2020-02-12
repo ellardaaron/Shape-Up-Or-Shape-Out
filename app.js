b2116f60e6caabd48d896b58e7c9c3d28b4b6dd2
ellardaaron <59664272+ellardaaron@users.noreply.github.com>
2020-02-11 22:02:46 -0600

    completed but working on making it pretty

--- app.js
+++ app.js
@@ -1,59 +1,65 @@
-const MAX = 600; //sets the max pixels for the position the the div will be placed inside the square div
+//Variables----------------------
 
-let container = document.getElementById('shapeCanvas'); //sets the variable container to the square container div in the html
+let shapeCanvas = document.getElementById('shape-canvas'); //sets the variable shapeCanvas to the shapeCanvas div that is in the html
 
-//get info 
-let shapeName = document.getElementById('shape-name');
-let shapeWidth = document.getElementById('width-info');
-let shapeHeight = document.getElementById('height-info');
-let shapeRadius = document.getElementById('radius-info');
-let shapeArea = document.getElementById('area-info');
-let shapeParameter = document.getElementById('parameter-info');
+
+let shapeName = document.getElementById('shape-name');              // \
+let shapeWidth = document.getElementById('width-info');             //  \
+let shapeHeight = document.getElementById('height-info');           //   \  sets variables to the text input fields from the html
+let shapeRadius = document.getElementById('radius-info');           //   /  so the values can be changed within the click listeners
+let shapeArea = document.getElementById('area-info');               //  /   that are inside of the separate shape constructors
+let shapePerimeter = document.getElementById('perimeter-info');     // /
 
 
 //Shape Constructors-------------
 
-class Shape {
-    constructor(x,y){
-        this.div = document.createElement('div'); //creates a new div for the shape
-        this.div.style.left = `${x}px`;           //sets x coordinate for the shape to be placed
-        this.div.style.top = `${y}px`;            //sets y coordinate for the shape to be placed
-        
+class Shape {                                      // the main class
+    constructor(x,y){                              // the main constructor
+        this.div = document.createElement('div');  // creates a new div for the shape
+        this.div.style.left = `${x}px`;            // sets x coordinate for the shape to be placed
+        this.div.style.top = `${y}px`;             // sets y coordinate for the shape to be placed
+        shapeCanvas.appendChild(this.div);         // appends the shape as a child to the shapeCanvas 
     }
 }
-class Square extends Shape {
-    constructor (x, y, size){
-        super(x,y);                             //super calls the information for placement that has been determined in the main shape constructor
-        this.div.classList.add('square');          // adds a css class 
-        this.div.style.width = `${size}px`;         //setting the width to the size. Size is the amount the user enters in the square-input submit field in the index. square input is then set to submit 
-        this.div.style.height = `${size}px`;        
-        container.appendChild(this.div);
-        
+class Square extends Shape {                       // class for the square that will extend the shape class
+    constructor (x,y,size){                        // x and y for the coordinates and size is what the user enters. In the function insertSquare the number that the user enters in square-input is set to 
+        super(x,y);                                // super calls the information for placement that has been determined in the main shape constructor
+        this.div.classList.add('square');          // adds a css class so it can be referenced in the css
+        this.div.style.width = `${size}px`;        // setting the width to size value. Size is the amount the user enters in the square-input submit field in the index. In the function the value from square-input will be set to size.
+        this.div.style.height = `${size}px`;       // setting the height to size value as well being that on a square they are the same
+
         this.div.addEventListener ('click', () => {
             shapeName.value=this.div.classList;
             shapeWidth.value=size;
             shapeHeight.value=size;
             shapeRadius.value="Not Applicable";
             shapeArea.value=size**2;
-            shapeParameter.value=4*size;
+            shapePerimeter.value=4*size;
+        })
+
+        this.div.addEventListener ('dblclick', () => {
+            this.div.remove();
         })
     }
 }
 class Circle extends Shape {
     constructor (x, y, radius){
-        super(x,y);                     //super calls the information for placement that has been determined in the main shape constructor
-        this.div.classList.add('circle');          // adds a css class 
-        this.div.style.width = `${radius}px`;         
-        this.div.style.height = `${radius}px`;        
-        container.appendChild(this.div);        
-        
+        super(x,y);                    
+        this.div.classList.add('circle');         
+        this.div.style.width = `${radius*2}px`;         
+        this.div.style.height = `${radius*2}px`;        
+
         this.div.addEventListener ('click', () => {
             shapeName.value=this.div.classList;
-            shapeWidth.value=radius;
-            shapeHeight.value=radius;
-            shapeRadius.value=radius/2;
-            shapeArea.value=(Math.PI)*((radius/2)**2);
-            shapeParameter.value=(2)*(radius/2)*(Math.PI);
+            shapeWidth.value=radius*2;
+            shapeHeight.value=radius*2;
+            shapeRadius.value=radius;
+            shapeArea.value=(Math.PI)*((radius)**2);
+            shapePerimeter.value=(2)*(radius)*(Math.PI);
+        })
+
+        this.div.addEventListener ('dblclick', () => {
+            this.div.remove();
         })
     }
 }
@@ -63,25 +69,32 @@ class Rectangle extends Shape {
         this.div.classList.add('rectangle');
         this.div.style.height = `${recHeight}px`; 
         this.div.style.width = `${recWidth}px`;
-        container.appendChild(this.div);
-        
+
         this.div.addEventListener ('click', () => {
             shapeName.value=this.div.classList;
             shapeWidth.value=recWidth;
             shapeHeight.value=recHeight;
             shapeRadius.value="Not Applicable";
             shapeArea.value=recWidth*recHeight;
-            shapeParameter.value= 2*(+recWidth + +recHeight);
+            shapePerimeter.value= 2*(+recWidth + +recHeight);
         })
+
+        this.div.addEventListener ('dblclick', () => {
+            this.div.remove();
+        })
+
+        if (recHeight === recWidth){
+            alert('This is a square, not a rectangle!');
+            this.div.remove();
+        }
     }
 }
 class Triangle extends Shape {
     constructor (x, y, size){
         super(x,y);
         this.div.classList.add('triangle');         
-        this.div.style.borderBottom= `${size}px solid red`;        
+        this.div.style.borderBottom= `${size}px solid yellow`;        
         this.div.style.borderRight = `${size}px solid transparent`;        
-        container.appendChild(this.div);
 
         this.div.addEventListener ('click', () => {
             shapeName.value=this.div.classList;
@@ -89,15 +102,19 @@ class Triangle extends Shape {
             shapeHeight.value=size;
             shapeRadius.value="Not Applicable";
             shapeArea.value=(size*size)/2;
-            shapeParameter.value= +size + +size + Math. sqrt(size**2 + size**2) ;
+            shapePerimeter.value= +size + +size + Math. sqrt(size**2 + size**2) ;
+        })
+
+        this.div.addEventListener ('dblclick', () => {
+            this.div.remove();
         })
     }
 }
 
 //Buttons---------------
 
-let sqButton = document.getElementById('sq-button'); //assigns sqButton to the html button "sq-button"
-sqButton.addEventListener('click', insertSquare);       //adds an event listener that when clicked executes the insert square function
+let sqButton = document.getElementById('sq-button'); 
+sqButton.addEventListener('click', insertSquare);       
 
 let cirButton = document.getElementById('cir-button');
 cirButton.addEventListener('click', insertCircle); 
@@ -111,41 +128,36 @@ triButton.addEventListener('click', insertTriangle);
 //Functions-------------
 
 function insertSquare () {
-    var submitValue = document.getElementById("square-input").value;
-    let xVal = randomVal(0,MAX-submitValue);  //calls on randomVal function. picks a number from 0-800 because MAX is set to 800 px
-    let yVal = randomVal(0,MAX-submitValue);
-    let size = submitValue;
+    var size = document.getElementById("square-input").value;
+    let xVal = randomVal(0,600-size); 
+    let yVal = randomVal(0,600-size);
     let sq = new Square(xVal, yVal, size);
 }
 
 function insertCircle () {
-    var submitValue = document.getElementById("circle-input").value;
-    let xVal = randomVal(0,MAX-(submitValue*2));  
-    let yVal = randomVal(0,MAX-(submitValue*2));
-    let radius = submitValue*2;                  // multiply the submit value by 2 because radius is only half of the circumference
+    var radius = document.getElementById("circle-input").value;
+    let xVal = randomVal(0,600-(radius*2));  
+    let yVal = randomVal(0,600-(radius*2));
     let cir = new Circle(xVal, yVal, radius);
 }
 
 function insertRectangle () {
-    var heightValue = document.getElementById("rectangle-height-input").value;
-    var widthValue = document.getElementById("rectangle-width-input").value;
-    let xVal = randomVal(0,MAX-widthValue); 
-    let yVal = randomVal(0,MAX-heightValue);
-    let recHeight = heightValue;
-    let recWidth = widthValue;
+    var recHeight = document.getElementById("rectangle-height-input").value;
+    var recWidth = document.getElementById("rectangle-width-input").value;
+    let xVal = randomVal(0,600-recWidth); 
+    let yVal = randomVal(0,600-recHeight);
     let rec = new Rectangle(xVal, yVal, recHeight, recWidth);
 
 }
 
 function insertTriangle () {
-    var submitValue = document.getElementById("triangle-input").value;
-    let xVal = randomVal(0,MAX-submitValue);  //calls on randomVal function. picks a number from 0-800 because MAX is set to 800 px
-    let yVal = randomVal(0,MAX-submitValue);
-    let size = submitValue;
+    var size = document.getElementById("triangle-input").value;
+    let xVal = randomVal(0,600-size);  
+    let yVal = randomVal(0,600-size);
     let tri = new Triangle(xVal, yVal, size);
 }
 
 
-function randomVal (min,max) {    //a random value function that pics a number between the inserted min and max values
+function randomVal (min,max) {    
     return Math.floor(Math.random() * (max - min));
 }
